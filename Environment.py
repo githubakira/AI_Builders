c45cfff1a1ee0e9b73e27324344834bf4f8b94b8
githubakira <69522247+githubakira@users.noreply.github.com>
2021-10-26 23:23:34 +0700

    Update Environment.py
    def player(b,indx,items,mainBoard):

--- Environment.py
+++ Environment.py
@@ -163,7 +163,7 @@ def dboard(mainBoard,items,N=6):
       items[kk].style.button_color = 'white'
   return
 
-def player(b,indx,items):
+def player(b,indx,items,mainBoard):
   if makeMove(mainBoard, playerTile, indx[0], indx[1]):
     dboard(mainBoard,items)
     if gameEnd(mainBoard):
